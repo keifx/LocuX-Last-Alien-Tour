013a11931cd9c471508f04d6aa0c360308528cfa
keifx <cangkumis@gmail.com>
2024-02-25 10:49:38 +0700

    Local Refactoring
    Refactor all Assets by Locally remove and add new asset.
    this happens because i didn't directly interact with the git directly from unity hub

--- Assets/Scripts/ManagerScr.cs
+++ Assets/Scripts/ManagerScr.cs
@@ -8,58 +8,79 @@ public class ManagerScr : MonoBehaviour
     public GameObject cube;
     public GameObject fallCube;
     public GameObject pivotCamera;
+    public GameObject yHeightReference;
 
-    public GameObject yHeight;
-    public int pengali;
-    public float timer;
-    public Quaternion targetRotCamera ;
+    public int squareSize;
+    private float timerFallingCube;
+    public float timerMultiplier;
+    private Quaternion targetRotCamera;
+    public int sumbuX;
     public int sumbuY;
+
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI deathScoreText;
+
     void Start()
     {
-        for (int i = 0; i < pengali; i++)
-        {
+        //squareSize = 4;
+        //sumbuX = 35;
+        //sumbuY = 45;
 
-            for (int j = 0; j < pengali; j++)
-            {
-                var cubeFloor = Instantiate(cube, new Vector3(j, 0, i), Quaternion.identity);
-            }
-        }
-        sumbuY = 45;
+        GenerateBaseCube(squareSize, cube);
+    }
+
+    //all here are update per frame
+    // so it won't stack
+    private void FixedUpdate()
+    {
+        targetRotCamera = Quaternion.Euler(sumbuX, sumbuY, 0);
+        pivotCamera.transform.localRotation = Quaternion.Slerp(pivotCamera.transform.localRotation, targetRotCamera, 15 * Time.deltaTime);
     }
 
     void Update()
     {
-        if(timer > 0)
+        //camera falling frequency
+        if(timerFallingCube > 0)
         {
-            
-            timer -= Time.deltaTime;
+            timerFallingCube -= Time.deltaTime;
         }
         else
         {
-            //Function
             SpawnFallCube();
-            timer = 0.5f;
+            timerFallingCube = 0.5f;
         }
 
+        //camera Y lock the to the character character jump
         if (Input.GetKeyDown(KeyCode.RightArrow))
             sumbuY += 90;
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
             sumbuY -= 90;
-        targetRotCamera = Quaternion.Euler(30, sumbuY, 0);
-        pivotCamera.transform.localRotation = Quaternion.Slerp(pivotCamera.transform.localRotation, targetRotCamera, 15 * Time.deltaTime);
 
-        if (int.Parse(scoreText.text) < Mathf.Round(yHeight.transform.localPosition.y))
+        //score update
+        if (int.Parse(scoreText.text) < Mathf.Round(yHeightReference.transform.localPosition.y))
         {
-            scoreText.text = Mathf.Round(yHeight.transform.localPosition.y).ToString();
+            scoreText.text = Mathf.Round(yHeightReference.transform.localPosition.y).ToString();
         }
         deathScoreText.text = scoreText.text;
+    }
+
+    private void GenerateBaseCube(int size, GameObject g)
+    {
+        //generate base plane
+        //j for x
+        //i for z
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                var cubeFloor = Instantiate(g, new Vector3(j, 0, i), Quaternion.identity);
+            }
+        }
     }
     
     public void SpawnFallCube()
     { 
-        var spawnedCube = Instantiate(fallCube, new Vector3(Random.Range(0,pengali), 100, Random.Range(0, pengali)), Quaternion.identity);
+        var spawnedCube = Instantiate(fallCube, new Vector3(Random.Range(0,squareSize), 100, Random.Range(0, squareSize)), Quaternion.identity);
     }
 }
